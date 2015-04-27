d584e93af69569fe2c725f602bf01132acd0dc22
Sarah R <phoesarah@hotmail.com>
2015-04-27 16:58:40 -0500

    Complete fibonacci program

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -13,20 +13,22 @@ namespace Fibonacci
             int firstnumber = 1;
             int secondnumber = 0;
             int totalfirstsecond = 0;
+            int sum = 0;
 
-            while (totalfirstsecond < 10)
+            while (totalfirstsecond < 4000000)
             {
-                Console.Write(Convert.ToString(totalfirstsecond));
+                Console.Write(" " + Convert.ToString(totalfirstsecond));
                 totalfirstsecond = firstnumber + secondnumber;
                 firstnumber = secondnumber;
                 secondnumber = totalfirstsecond;
                 
                 if (totalfirstsecond % 2 == 0)
                 {
-                
+                    sum = totalfirstsecond + sum;
                 }
 
             }
+            Console.WriteLine(" \n The Sum of all even numbers in this sequence is " + Convert.ToString(sum));
             Console.ReadLine();
 
         }
