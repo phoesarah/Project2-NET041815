c67c244eac1c0c52bed690de637cb926f22e2816
Sarah R <phoesarah@hotmail.com>
2015-04-27 16:41:16 -0500

    Fibonacci sequence working

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 1;
+            int secondnumber = 0;
+            int totalfirstsecond = 0;
+
+            while (totalfirstsecond < 10)
+            {
+                Console.Write(Convert.ToString(totalfirstsecond));
+                totalfirstsecond = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = totalfirstsecond;
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
